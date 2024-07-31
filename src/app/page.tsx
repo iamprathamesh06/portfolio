bcb7a585cea44f3a4fcfd95aee4c4f294d8d335c
Prathamesh Moharkar <74674406+iamprathamesh06@users.noreply.github.com>
2024-07-31 08:36:58 +0000

    Experiment with sidebar glassmorphism

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,13 @@ export default function Home() {
   return (
     <>
       <StarsCanvas />
-      <main className="z-10 absolute w-full border-2 border-white">
-        <h1>Hello</h1>
-        {/* Content will go here */}
+      <main className="z-10 absolute w-full  ">
+        <section className="flex">
+          <nav className="flex-[2] h-screen w-full bg-gray-0 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-0 shadow-md shadow-gray-50 mx-5"></nav>
+          <div className="flex-[8] h-[1000px] ">
+            <h1>Hello</h1>
+          </div>
+        </section>
       </main>
     </>
   );
